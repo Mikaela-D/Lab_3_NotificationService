3b5a63a63de1c32ea09a35041ee8e448ea655302
Mikaela-D <G00401392@atu.ie>
2023-10-20 20:11:57 +0100

    Created new project NotificationService

---
+++ src/main/java/ie/atu/lab3_notificationservice/Lab3NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3_notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab3_notificationservice/Lab3NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3_notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
